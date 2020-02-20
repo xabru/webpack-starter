5155725cacfd73a63972655acc74b6dd76fde4c6
xabru <55867760+xabru@users.noreply.github.com>
2020-02-19 20:54:34 -0500

    change to dev config webpack

--- webpack.config.js
+++ webpack.config.js
@@ -7,8 +7,8 @@ const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
 
-    // mode: 'development',
-    mode: 'production',
+    mode: 'development',
+    // mode: 'production',
 
     optimization: {
         minimizer: [ new OptimizeCssAssetsPlugin()]
